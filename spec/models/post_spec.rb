51d398127063e9e57fd305aff5064966095c67ed
Camila Achutti <camila.achutti@gmail.com>
2014-03-06 11:58:06 -0300

    Validates post title and body

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should not be created without a title" do
+    post = Post.new(body: "example body", title: "")
+    post.should have(1).error_on(:title)
+  end
+
+  it "should not be created without a body" do
+    post = Post.new(title: "example title", body: "")
+    post.should have(1).error_on(:body)
+  end
+
+  it "should not be created without duplicated title" do
+    first_post = Post.create(title: "Unique title", body: "Example body")
+    post = Post.new(title: first_post.title, body: "Another body")
+    post.should have(1).error_on(:title)
+  end  
 end
